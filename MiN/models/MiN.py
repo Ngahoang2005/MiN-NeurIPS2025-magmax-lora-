dc07c9188216abffc061fdf8203e8a56858e67af
Ngahoang2005 <hoangthanhnga2005@gmail.com>
2026-02-05 19:41:56 +0700

    d

--- MiN/models/MiN.py
+++ MiN/models/MiN.py
@@ -2,19 +2,21 @@ import math, random, gc, os
 import numpy as np
 from tqdm import tqdm
 import torch
+from torch import optim
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, Subset
 from utils.inc_net import MiNbaseNet
-from utils.toolkit import calculate_class_metrics, calculate_task_metrics
+from utils.toolkit import tensor2numpy, calculate_class_metrics, calculate_task_metrics
 from utils.training_tool import get_optimizer, get_scheduler
 from torch.amp import autocast, GradScaler
 
 class MinNet(object):
     def __init__(self, args, loger):
         super().__init__()
-        self.args, self.logger, self.device = args, loger, args['device']
+        self.args, self.logger = args, loger
+        self.device = args['device']
         self._network = MiNbaseNet(args).to(self.device)
-        self.num_workers = args["num_workers"] # Set về 0 trong config nếu vẫn treo
+        self.num_workers = args["num_workers"]
         self.init_epochs, self.init_lr = args["init_epochs"], args["init_lr"]
         self.lr, self.batch_size = args["lr"], args["batch_size"]
         self.init_class, self.increment = args["init_class"], args["increment"]
@@ -43,108 +45,162 @@ class MinNet(object):
 
     def init_train(self, data_manger):
         self.cur_task += 1
-        train_list, _, _ = data_manger.get_task_list(0)
+        train_list, test_list, train_list_name = data_manger.get_task_list(0)
+        self.logger.info(f"task_list: {train_list_name}")
+        self.logger.info(f"task_order: {train_list}")
+
         train_set = data_manger.get_task_data(source="train", class_list=train_list)
         train_set.labels = self.cat2order(train_set.labels, data_manger)
+        test_set = data_manger.get_task_data(source="test", class_list=test_list)
+        test_set.labels = self.cat2order(test_set.labels, data_manger)
+
+        train_loader = DataLoader(train_set, batch_size=self.init_batch_size, shuffle=True, num_workers=self.num_workers)
+        self.test_loader = DataLoader(test_set, batch_size=self.init_batch_size, shuffle=False, num_workers=self.num_workers)
+
         if self.args['pretrained']:
             for param in self._network.backbone.parameters(): param.requires_grad = True
+
         self._network.update_fc(self.init_class)
         self._network.update_noise()
+        self._clear_gpu()
         
-        self.run(DataLoader(train_set, batch_size=self.args['init_batch_size'], shuffle=True, num_workers=self.num_workers))
-        self._network.collect_projections(val=0.95)
-        self.fit_fc(DataLoader(train_set, batch_size=self.buffer_batch, shuffle=True, num_workers=self.num_workers), None)
+        self.run(train_loader)
+        self._network.collect_projections(mode='threshold', val=0.95)
+        self._clear_gpu()
         
-        train_no_aug = data_manger.get_task_data(source="train_no_aug", class_list=train_list)
-        train_no_aug.labels = self.cat2order(train_no_aug.labels, data_manger)
+        # [GIỮ NGUYÊN]: Fit FC bằng train_set
+        fit_loader = DataLoader(train_set, batch_size=self.buffer_batch, shuffle=True, num_workers=self.num_workers)
+        self.fit_fc(fit_loader, self.test_loader)
+
+        # [GIỮ NGUYÊN]: Re-fit bằng train_no_aug
+        train_set_no_aug = data_manger.get_task_data(source="train_no_aug", class_list=train_list)
+        train_set_no_aug.labels = self.cat2order(train_set_no_aug.labels, data_manger)
+        train_loader_no_aug = DataLoader(train_set_no_aug, batch_size=self.buffer_batch, shuffle=True, num_workers=self.num_workers)
+
         if self.args['pretrained']:
             for param in self._network.backbone.parameters(): param.requires_grad = False
-        self.re_fit(DataLoader(train_no_aug, batch_size=self.buffer_batch, shuffle=True, num_workers=self.num_workers), None)
+
+        self.re_fit(train_loader_no_aug, self.test_loader)
+        del train_set, test_set
+        self._clear_gpu()
 
     def increment_train(self, data_manger):
         self.cur_task += 1
-        self._old_network = copy.deepcopy(self._network).to(self.device).eval()
-        train_list, _, _ = data_manger.get_task_list(self.cur_task)
+        train_list, test_list, train_list_name = data_manger.get_task_list(self.cur_task)
+        self.logger.info(f"task_list: {train_list_name}")
+        self.logger.info(f"task_order: {train_list}")
+
         train_set = data_manger.get_task_data(source="train", class_list=train_list)
         train_set.labels = self.cat2order(train_set.labels, data_manger)
+        test_set = data_manger.get_task_data(source="test", class_list=test_list)
+        test_set.labels = self.cat2order(test_set.labels, data_manger)
+
+        train_loader = DataLoader(train_set, batch_size=self.buffer_batch, shuffle=True, num_workers=self.num_workers)
+        self.test_loader = DataLoader(test_set, batch_size=self.buffer_batch, shuffle=False, num_workers=self.num_workers)
+
+        self._old_network = copy.deepcopy(self._network).to(self.device).eval()
         if self.args['pretrained']:
             for param in self._network.backbone.parameters(): param.requires_grad = False
 
-        self.fit_fc(DataLoader(train_set, batch_size=self.buffer_batch, shuffle=True, num_workers=self.num_workers), None)
+        # [GIỮ NGUYÊN]: Fit trước
+        self.fit_fc(train_loader, self.test_loader)
         self._network.update_fc(self.increment)
-        self._network.update_noise()
-        self.run(DataLoader(train_set, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers))
-        self._network.collect_projections(val=0.95)
         
-        train_no_aug = data_manger.get_task_data(source="train_no_aug", class_list=train_list)
-        train_no_aug.labels = self.cat2order(train_no_aug.labels, data_manger)
-        self.re_fit(DataLoader(train_no_aug, batch_size=self.buffer_batch, shuffle=True, num_workers=self.num_workers), None)
+        run_loader = DataLoader(train_set, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+        self._network.update_noise()
+        self._clear_gpu()
+
+        self.run(run_loader)
+        self._network.collect_projections(mode='threshold', val=0.95)
+        self._clear_gpu()
+
+        del train_set
+        train_set_no_aug = data_manger.get_task_data(source="train_no_aug", class_list=train_list)
+        train_set_no_aug.labels = self.cat2order(train_set_no_aug.labels, data_manger)
+        train_loader_no_aug = DataLoader(train_set_no_aug, batch_size=self.buffer_batch, shuffle=True, num_workers=self.num_workers)
+
+        # [GIỮ NGUYÊN]: Re-fit sau
+        self.re_fit(train_loader_no_aug, self.test_loader)
+        del train_set_no_aug, test_set
+        self._clear_gpu()
 
     def fit_fc(self, train_loader, test_loader):
+        """Fit FC: Logic DPCR thay thế bên trong vòng lặp của mày"""
         self._network.eval()
         dataset = train_loader.dataset
         classes = sorted(list(set(dataset.labels)))
+        # [DPCR]: Gom và nén theo từng class để tiết kiệm VRAM
         for c in classes:
             idx = np.where(np.array(dataset.labels) == c)[0]
             sub = DataLoader(Subset(dataset, idx), batch_size=self.buffer_batch)
             for _ in range(self.fit_epoch):
                 for _, x, y in sub:
-                    self._network.accumulate_stats(x, F.one_hot(y.to(self.device), self._network.known_class))
+                    y_oh = F.one_hot(y.to(self.device), self._network.known_class)
+                    self._network.fit(x, y_oh)
             self._network.compress_stats(); self._clear_gpu()
-        self._network.solve_dpcr(init_mode=(self.cur_task==0))
+        self._network.solve_analytic(init_mode=(self.cur_task==0))
 
     def re_fit(self, train_loader, test_loader):
+        """Re-fit: Có Drift Correction theo chuẩn DPCR"""
         P = self.calculate_drift(train_loader) if self.cur_task > 0 else None
         boundary = self.known_class - self.increment if self.cur_task > 0 else 0
         for _, x, y in train_loader:
-            self._network.accumulate_stats(x, F.one_hot(y.to(self.device), self._network.known_class))
-        self._network.solve_dpcr(P_drift=P, boundary=boundary, init_mode=False)
+            y_oh = F.one_hot(y.to(self.device), self._network.known_class)
+            self._network.fit(x, y_oh)
+        self._network.solve_analytic(P_drift=P, boundary=boundary, init_mode=False)
         self._clear_gpu()
 
     def run(self, train_loader):
-        params = filter(lambda p: p.requires_grad, self._network.parameters())
-        opt = get_optimizer(self.args['optimizer_type'], params, (self.init_lr if self.cur_task==0 else self.lr), self.args['weight_decay'])
-        sch = get_scheduler(self.args['scheduler_type'], opt, (self.init_epochs if self.cur_task==0 else self.epochs))
+        # [GIỮ NGUYÊN]: Toàn bộ logic train Gradient Descent của mày
+        epochs = self.init_epochs if self.cur_task == 0 else self.epochs
+        optimizer = get_optimizer(self.args['optimizer_type'], filter(lambda p: p.requires_grad, self._network.parameters()), 
+                                  (self.init_lr if self.cur_task==0 else self.lr), self.args['weight_decay'])
+        scheduler = get_scheduler(self.args['scheduler_type'], optimizer, epochs)
         self._network.train()
-        for epoch in range(self.init_epochs if self.cur_task==0 else self.epochs):
-            for _, x, y in train_loader:
-                x, y = x.to(self.device), y.to(self.device)
-                opt.zero_grad()
+        for epoch in range(epochs):
+            for _, inputs, targets in train_loader:
+                inputs, targets = inputs.to(self.device), targets.to(self.device)
+                optimizer.zero_grad()
                 with autocast('cuda'):
-                    l2 = self._network.forward_normal_fc(x)['logits']
+                    l2 = self._network.forward_normal_fc(inputs)['logits']
                     if self.cur_task > 0:
-                        with torch.no_grad(): l1 = self._network(x)['logits']
+                        with torch.no_grad(): l1 = self._network(inputs)['logits']
                         if l2.shape[1] > l1.shape[1]:
                             pad = torch.zeros((l1.shape[0], l2.shape[1]-l1.shape[1]), device=self.device, dtype=l1.dtype)
                             l1 = torch.cat([l1, pad], dim=1)
                         logits = l2 + l1.to(l2.dtype)
                     else: logits = l2
-                    loss = F.cross_entropy(logits, y.long())
+                    loss = F.cross_entropy(logits, targets.long())
                 self.scaler.scale(loss).backward()
                 if self.cur_task > 0: 
-                    self.scaler.unscale_(opt); self._network.apply_gpm_to_grads(scale=0.85)
-                self.scaler.step(opt); self.scaler.update()
-            sch.step()
+                    self.scaler.unscale_(optimizer); self._network.apply_gpm_to_grads(scale=0.85)
+                self.scaler.step(optimizer); self.scaler.update()
+            scheduler.step()
         self._clear_gpu()
 
     def after_train(self, data_manger):
+        # [GIỮ NGUYÊN]: In kết quả acc/avg_acc theo mẫu mày gửi
         if self.cur_task == 0: self.known_class = self.init_class
         else: self.known_class += self.increment
         _, test_list, _ = data_manger.get_task_list(self.cur_task)
         test_set = data_manger.get_task_data(source="test", class_list=test_list)
         test_set.labels = self.cat2order(test_set.labels, data_manger)
-        res = self.eval_task(DataLoader(test_set, batch_size=self.init_batch_size, num_workers=0))
-        self.total_acc.append(round(float(res['all_class_accy']*100.), 2))
-        print(f'total acc: {self.total_acc}\navg_acc: {np.mean(self.total_acc):.2f}')
+        eval_res = self.eval_task(DataLoader(test_set, batch_size=self.init_batch_size, num_workers=0))
+        self.total_acc.append(round(float(eval_res['all_class_accy']*100.), 2))
+        
         self.logger.info(f'total acc: {self.total_acc}')
-        self.logger.info(f'avg_acc: {np.mean(self.total_acc):.2f}') 
-        print('avg_acc:', np.mean(self.total_acc))
-    def eval_task(self, loader):
-        self._network.eval(); pred, label = [], []
+        self.logger.info(f'avg_acc: {np.mean(self.total_acc):.2f}')
+        print(f'total acc: {self.total_acc}')
+        print(f'avg_acc: {np.mean(self.total_acc):.2f}')
+        self._clear_gpu()
+
+    def eval_task(self, test_loader):
+        self._network.eval()
+        pred, label = [], []
         with torch.no_grad():
-            for _, x, y in loader:
-                logits = self._network(x.to(self.device))["logits"]
-                pred.extend(torch.max(logits, 1)[1].cpu().numpy()); label.extend(y.numpy())
+            for _, inputs, targets in test_loader:
+                logits = self._network(inputs.to(self.device))["logits"]
+                pred.extend(torch.max(logits, 1)[1].cpu().numpy()); label.extend(targets.numpy())
         res = calculate_class_metrics(pred, label)
         task_res = calculate_task_metrics(pred, label, self.init_class, self.increment)
         return {"all_class_accy": res['all_accy'], "task_confusion": task_res['task_confusion_matrices']}
--- MiN/utils/inc_net.py
+++ MiN/utils/inc_net.py
@@ -2,12 +2,35 @@ import copy
 import torch
 from torch import nn
 from torch.nn import functional as F
-try:
-    from torch.amp import autocast
-except ImportError:
-    from torch.cuda.amp import autocast 
 from backbones.pretrained_backbone import get_pretrained_backbone
 from backbones.linears import SimpleLinear
+from torch.cuda.amp import autocast 
+
+class BaseIncNet(nn.Module):
+    def __init__(self, args: dict):
+        super(BaseIncNet, self).__init__()
+        self.args = args
+        self.backbone = get_pretrained_backbone(args)
+        self.feature_dim = self.backbone.out_dim
+        self.fc = None
+
+    def update_fc(self, nb_classes):
+        fc = self.generate_fc(self.feature_dim, nb_classes)
+        if self.fc is not None:
+            nb_output = self.fc.out_features
+            fc.weight.data[:nb_output] = copy.deepcopy(self.fc.weight.data)
+            fc.bias.data[:nb_output] = copy.deepcopy(self.fc.bias.data)
+        del self.fc
+        self.fc = fc
+
+    @staticmethod
+    def generate_fc(in_dim, out_dim):
+        return SimpleLinear(in_dim, out_dim)
+
+    def forward(self, x):
+        hyper_features = self.backbone(x)
+        logits = self.fc(hyper_features)['logits']
+        return {'features': hyper_features, 'logits': logits}
 
 class RandomBuffer(torch.nn.Linear):
     def __init__(self, in_features: int, buffer_size: int, device):
@@ -15,12 +38,14 @@ class RandomBuffer(torch.nn.Linear):
         self.bias = None
         self.in_features, self.out_features = in_features, buffer_size
         factory_kwargs = {"device": device, "dtype": torch.float32}
-        self.register_buffer("weight", torch.empty((self.in_features, self.out_features), **factory_kwargs))
-        nn.init.normal_(self.weight, std=0.02)
+        self.W = torch.empty((self.in_features, self.out_features), **factory_kwargs)
+        self.register_buffer("weight", self.W)
+        self.reset_parameters()
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
-        # Ép X về device/type của weight (GPU/Float32)
-        return F.relu(X.to(device=self.weight.device, dtype=self.weight.dtype) @ self.weight)
+        # Ép về float32/GPU để đồng bộ
+        X = X.to(device=self.weight.device, dtype=self.weight.dtype)
+        return F.relu(X @ self.weight)
 
 class MiNbaseNet(nn.Module):
     def __init__(self, args: dict):
@@ -31,36 +56,41 @@ class MiNbaseNet(nn.Module):
         self.feature_dim = self.backbone.out_dim 
 
         self.buffer = RandomBuffer(self.feature_dim, self.buffer_size, self.device)
+        
+        # [DPCR]: Thay R bằng storage Dictionary
         self.register_buffer("weight", torch.zeros((self.buffer_size, 0), device=self.device, dtype=torch.float32))
-
-        # DPCR Storage 100% trên GPU
         self._compressed_stats = {} 
         self._mu_list = {}          
         self._class_counts = {}     
         self.temp_phi, self.temp_mu, self.temp_count = {}, {}, {}
 
-        self.normal_fc = None
-        self.cur_task, self.known_class = -1, 0
+        self.normal_fc, self.cur_task, self.known_class = None, -1, 0
 
     def update_fc(self, nb_classes):
         self.cur_task += 1
         self.known_class += nb_classes
-        new_fc = SimpleLinear(self.buffer_size, self.known_class, bias=(self.cur_task==0)).to(self.device)
+        if self.cur_task > 0:
+            new_fc = SimpleLinear(self.buffer_size, self.known_class, bias=False)
+        else:
+            new_fc = SimpleLinear(self.buffer_size, nb_classes, bias=True)
+            
         if self.normal_fc is not None:
-            new_fc.weight.data[:self.normal_fc.out_features] = self.normal_fc.weight.data
+            old_nb_output = self.normal_fc.out_features
+            new_fc.weight.data[:old_nb_output] = self.normal_fc.weight.data
             if new_fc.bias is not None and self.normal_fc.bias is not None:
-                new_fc.bias.data[:self.normal_fc.out_features] = self.normal_fc.bias.data
-        self.normal_fc = new_fc
+                new_fc.bias.data[:old_nb_output] = self.normal_fc.bias.data
+        self.normal_fc = new_fc.to(self.device)
 
     @torch.no_grad()
-    def accumulate_stats(self, X: torch.Tensor, Y: torch.Tensor) -> None:
+    def fit(self, X: torch.Tensor, Y: torch.Tensor) -> None:
+        """Thay thế nội dung RLS bằng gom dữ liệu DPCR, giữ nguyên signature của mày"""
         self.eval()
-        # Đưa input lên GPU và ép float32 ngay lập tức
-        X, Y = X.to(self.device, dtype=torch.float32), Y.to(self.device, dtype=torch.float32)
-        
-        with autocast('cuda', enabled=False):
-            feat = self.buffer(self.backbone(X))
-        
+        X, Y = X.to(self.device), Y.to(self.device)
+        # Bắt buộc backbone/buffer chạy float32 để không bị nổ bias mismatch
+        with torch.cuda.amp.autocast(enabled=False):
+            feat = self.buffer(self.backbone(X.float()).float())
+            Y = Y.float()
+
         labels = torch.argmax(Y, dim=1)
         for i in range(feat.shape[0]):
             label = labels[i].item()
@@ -75,25 +105,20 @@ class MiNbaseNet(nn.Module):
 
     @torch.no_grad()
     def compress_stats(self):
-        """Nén trực tiếp trên GPU"""
+        """Hàm phụ trợ của DPCR để nén ma trận 8k/16k trên GPU"""
         RANK = 256
         for label in sorted(list(self.temp_phi.keys())):
-            raw_phi = self.temp_phi[label]
-            # Tính eigh thẳng trên GPU
-            S, V = torch.linalg.eigh(raw_phi)
-            
+            S, V = torch.linalg.eigh(self.temp_phi[label])
             S_top, V_top = (S[-RANK:], V[:, -RANK:]) if S.shape[0] > RANK else (S, V)
-            # Lưu kết quả cũng trên GPU luôn
             self._compressed_stats[label] = (V_top, S_top)
             self._mu_list[label] = (self.temp_mu[label] / self.temp_count[label])
             self._class_counts[label] = self.temp_count[label]
-            
-            del self.temp_phi[label], self.temp_mu[label], raw_phi
+            del self.temp_phi[label], self.temp_mu[label]
         torch.cuda.empty_cache()
 
     @torch.no_grad()
-    def solve_dpcr(self, P_drift=None, boundary=0, init_mode=False):
-        """Giải hệ phương trình 100% GPU"""
+    def solve_analytic(self, P_drift=None, boundary=0, init_mode=False):
+        """Bước giải hệ phương trình thay cho RLS cũ"""
         num_total = max(list(self._compressed_stats.keys()) + list(self.temp_phi.keys())) + 1
         if num_total > self.weight.shape[1]:
             new_w = torch.zeros((self.buffer_size, num_total), device=self.device)
@@ -114,26 +139,27 @@ class MiNbaseNet(nn.Module):
             total_phi += phi_c
             total_q[:, c] = mu_c * self._class_counts[c]
 
-        reg = self.gamma * torch.eye(self.buffer_size, device=self.device)
-        W = torch.linalg.solve(total_phi + reg, total_q)
-        if init_mode: 
-            self.normal_fc.weight.data[:W.shape[1]] = W.t().to(self.normal_fc.weight.dtype)
-        else: 
-            self.weight.data = F.normalize(W, p=2, dim=0)
+        for label, phi in self.temp_phi.items():
+            total_phi += phi
+            total_q[:, label] = self.temp_mu[label]
+
+        W = torch.linalg.solve(total_phi + self.gamma * torch.eye(self.buffer_size, device=self.device), total_q)
+        if init_mode: self.normal_fc.weight.data[:W.shape[1]] = W.t().to(self.normal_fc.weight.dtype)
+        else: self.weight.data = F.normalize(W, p=2, dim=0)
 
     def forward(self, x, new_forward=False):
         ref = next(self.backbone.parameters())
-        with autocast('cuda', enabled=False):
-            h = self.buffer(self.backbone(x.to(device=ref.device, dtype=ref.dtype), new_forward=new_forward))
+        with torch.cuda.amp.autocast(enabled=False):
+            h = self.buffer(self.backbone(x.to(ref.device, ref.dtype), new_forward=new_forward).float())
         return {'logits': h.to(self.weight.dtype) @ self.weight}
 
     def forward_normal_fc(self, x, new_forward=False):
         ref = next(self.backbone.parameters())
-        with autocast('cuda', enabled=False):
-            h = self.buffer(self.backbone(x.to(device=ref.device, dtype=ref.dtype), new_forward=new_forward))
+        with torch.cuda.amp.autocast(enabled=False):
+            h = self.buffer(self.backbone(x.to(ref.device, ref.dtype), new_forward=new_forward).float())
         return {"logits": self.normal_fc(h.to(self.normal_fc.weight.dtype))['logits']}
 
-    # Giữ nguyên các hàm noise/gpm
+    # Giữ nguyên các hàm mày yêu cầu
     def update_noise(self):
         for j in range(self.backbone.layer_num): self.backbone.noise_maker[j].update_noise()
     def unfreeze_noise(self):
@@ -141,11 +167,14 @@ class MiNbaseNet(nn.Module):
     def init_unfreeze(self):
         for j in range(len(self.backbone.noise_maker)):
             self.backbone.noise_maker[j].unfreeze_task_0()
-            for p in self.backbone.blocks[j].norm1.parameters(): p.requires_grad = True
-            for p in self.backbone.blocks[j].norm2.parameters(): p.requires_grad = True
+            if hasattr(self.backbone.blocks[j], 'norm1'):
+                for p in self.backbone.blocks[j].norm1.parameters(): p.requires_grad = True
+            if hasattr(self.backbone.blocks[j], 'norm2'):
+                for p in self.backbone.blocks[j].norm2.parameters(): p.requires_grad = True
         if hasattr(self.backbone, 'norm') and self.backbone.norm is not None:
             for p in self.backbone.norm.parameters(): p.requires_grad = True
     def collect_projections(self, mode='threshold', val=0.95):
         for j in range(self.backbone.layer_num): self.backbone.noise_maker[j].compute_projection_matrix(mode=mode, val=val)
     def apply_gpm_to_grads(self, scale=1.0):
         for j in range(self.backbone.layer_num): self.backbone.noise_maker[j].apply_gradient_projection(scale=scale)
+    def extract_feature(self, x): return self.backbone(x.to(self.device).float())
